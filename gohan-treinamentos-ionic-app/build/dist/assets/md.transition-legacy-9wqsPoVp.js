ad7864cf8076062aa4b855763a864c68588a896b
Pedro Victor <pedrovictor.rveras12@gmail.com>
2025-01-30 14:18:51 -0300

    up

--- gohan-treinamentos-ionic-app/build/dist/assets/md.transition-legacy-9wqsPoVp.js
+++ gohan-treinamentos-ionic-app/build/dist/assets/md.transition-legacy-9wqsPoVp.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C0c1IThs.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-CexEwoJV.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
