07908853176cb4b8222053ed0136b87b628144c0
Pedro Victor <pedrovictor.rveras12@gmail.com>
2025-02-18 04:53:38 -0300

    up

--- gohan-treinamentos-ionic-app/src/app/public/utils/dateUtils.ts
+++
@@ -1,9 +0,0 @@
-export const formatDate = () => {
-  const date = new Date();
-  return date.toLocaleDateString('pt-BR', {
-    weekday: 'long',
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  });
-};
